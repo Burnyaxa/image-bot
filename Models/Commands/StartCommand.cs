d36571a9cc98935101e9384801297c8784fb3fb2
Artem Supryhan <suprigan.artem1@gmail.com>
2020-10-18 21:01:01 +0300

    Add bot and start command

---
+++ Models/Commands/StartCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace image_bot.Models.Commands
+{
+    public class StartCommand : Command
+    {
+        public override string Name => @"/start";
+
+        public override bool Contains(Message message)
+        {
+            if (message.Type != Telegram.Bot.Types.Enums.MessageType.Text)
+                return false;
+
+            return message.Text.Contains(this.Name);
+        }
+
+        public override async Task Execute(Message message, TelegramBotClient botClient)
+        {
+            var chatId = message.Chat.Id;
+            await botClient.SendTextMessageAsync(chatId, "Hello and welcome to image-bot. Type '/' to see available commands.", parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown);
+        }
+    }
+}
