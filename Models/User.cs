b8183b7f89fda1a37c3a1fbdd82e2474a0c5d688
OmnixTry <Omnix.try@gmail.com>
2020-10-20 14:35:01 +0300

    Add user class and command enum.

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace image_bot.Models
+{
+    public enum Command
+    {
+        Resize,
+        ApplyFilter,
+        CreateMicroSticker
+    }
+    public class User
+    {
+        public int Id { get; set; }
+        
+        public int ChatId { get; set; }
+
+        public Command CurentCommand { get; set; }
+
+    }
+}
