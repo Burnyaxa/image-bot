f37b628e0e4560c92ed39fd995ecc4d5d42a1345
Artem Supryhan <suprigan.artem1@gmail.com>
2020-10-21 18:02:23 +0300

    Add base files

---
+++ Models/Commands/ImageResizeCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace image_bot.Models.Commands
+{
+    public class ImageResizeCommand : Command
+    {
+        public override string Name => @"/resize";
+
+        public override bool Contains(Message message)
+        {
+            if (message == null || message.Type != Telegram.Bot.Types.Enums.MessageType.Text)
+                return false;
+
+            return message.Text.Contains(this.Name);
+        }
+
+        public override async Task Execute(Message message, TelegramBotClient botClient)
+        {
+            var chatId = message.Chat.Id;
+        }
+    }
+}
---
+++ Models/ImageResizeRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace image_bot.Models
+{
+    public enum ImageResizeStatus
+    {
+        AwaitingImage,
+        AwaitingSize
+    }
+    public class ImageResizeRequest
+    {
+
+    }
+}
