e081080f42d38de2a37d1e453d43231390d20af7
Artem Supryhan <suprigan.artem1@gmail.com>
2020-10-21 18:56:57 +0300

    Add user controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using image_bot.Models;
+namespace image_bot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public UsersState db;
+        public UserController(UsersState context)
+        {
+            db = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(User user)
+        {
+            if(db.Users.Any(c => c.ChatId == user.ChatId))
+            {
+                return BadRequest();
+            }
+            db.Users.Add(user);
+            await db.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(User user)
+        {
+            if (db.Users.Any(c => c.ChatId == user.ChatId))
+            {
+                db.Users.Update(user);
+                await db.SaveChangesAsync();
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+
+    }
+}
