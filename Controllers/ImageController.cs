769282740be7e336d702a3fbf6e7d0237dd6e967
Artem Supryhan <suprigan.artem1@gmail.com>
2020-10-22 19:48:51 +0300

    Add set parameters

--- Controllers/ImageController.cs
+++ Controllers/ImageController.cs
@@ -4,13 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using image_bot.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace image_bot.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/image")]
     [ApiController]
     public class ImageController : ControllerBase
     {
+        public UsersState db;
+
+        public ImageController(UsersState context)
+        {
+            db = context;
+        }
+
+        [Route("set-parameters")]
+        [HttpPost]
+        public async Task<IActionResult> SetParameters(BotUser user, int height, int width)
+        {
+            ImageResizeRequest request = db.ImageResizeRequests.Include(u => u.User).Where(u => u.UserId == user.Id).First();
+            request.Height = height;
+            request.Width = width;
+            db.ImageResizeRequests.Update(request);
+            await db.SaveChangesAsync();
+            return Ok();
+        }
+
 
     }
 }
