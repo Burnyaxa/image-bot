b7919504e204188040c4ad06bd15b9e3224e61d5
Artem Supryhan <suprigan.artem1@gmail.com>
2020-10-25 15:24:10 +0200

    Add micro stickers command body and add it to Bot command list

---
+++ Models/Commands/CreateMircroStickersCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace image_bot.Models.Commands
+{
+    public class CreateMircroStickersCommand : Command
+    {
+        public override string Name => @"/micro-stickers";
+
+        public override bool Contains(Message message)
+        {
+            if (message == null || message.Type != Telegram.Bot.Types.Enums.MessageType.Text)
+                return false;
+
+            return message.Text.Contains(this.Name);
+        }
+
+        public override Task Execute(Message message, TelegramBotClient client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
