de7e58824c969a47640831e98fbb84a02282f27c
Artem Supryhan <suprigan.artem1@gmail.com>
2020-10-25 15:27:56 +0200

    Add MicroStickerController body

---
+++ Controllers/MicroStickerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using image_bot.Models;
+namespace image_bot.Controllers
+{
+    [Route("api/micro-sticker")]
+    [ApiController]
+    public class MicroStickerController : ControllerBase
+    {
+        public UsersState db;
+
+        public MicroStickerController(UsersState context)
+        {
+            db = context;
+        }
+
+
+    }
+}
