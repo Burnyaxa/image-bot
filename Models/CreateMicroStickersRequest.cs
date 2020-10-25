6fcd0a639af379243f8697cb4450764af2b76804
Artem Supryhan <suprigan.artem1@gmail.com>
2020-10-25 15:10:48 +0200

    Add microstrickers request model

---
+++ Models/CreateMicroStickersRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace image_bot.Models
+{
+    public enum MicroStickersStatus
+    {
+        AwaitingName,
+        AwaitingSticker
+    }
+    public class CreateMicroStickersRequest
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public BotUser User { get; set; }
+        /// <summary>
+        /// has default value inicial in UsersState
+        /// </summary>
+        public MicroStickersStatus Status { get; set; }
+        public string? Name { get; set; }
+        }
+}
